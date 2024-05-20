bc7eb6a878db5cdc5a4980ee9e6823f6e17566b7
Luigi <puccini.ua@gmail.com>
2024-05-20 15:54:59 +0300

    Changed fetchFunctions and appColors files to ts format

--- src/fetchFunctions.ts
+++ src/fetchFunctions.ts
@@ -5,24 +5,24 @@ export async function getPokemonList() {
   return response.json();
 }
 
-export async function getPokemonSpecieData(value) {
+export async function getPokemonSpecieData(value: string) {
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon-species/${value}`
   );
   return response.json();
 }
 
-export async function getPokemonFormData(value) {
+export async function getPokemonFormData(value: string) {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${value}`);
   return response.json();
 }
 
-export async function getAbilityData(text) {
+export async function getAbilityData(text: string) {
   const response = await fetch(`https://pokeapi.co/api/v2/ability/${text}`);
   return response.json();
 }
 
-export async function getTypeData(value) {
+export async function getTypeData(value: string) {
   const response = await fetch(`https://pokeapi.co/api/v2/type/${value}`);
   return response.json();
 }
